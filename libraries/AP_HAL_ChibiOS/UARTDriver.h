ee66f08ed30ca0381890fcaf62a7bc73a8a9603b
Mark Whitehorn <kd0aij@gmail.com>
2018-01-23 14:25:20 -0700

    AP_HAL_Chibios: fix merge conflict

--- libraries/AP_HAL_ChibiOS/UARTDriver.h
+++ libraries/AP_HAL_ChibiOS/UARTDriver.h
@@ -124,5 +124,4 @@ private:
     void write_pending_bytes_NODMA(uint32_t n);
     void write_pending_bytes_DMA_from_irq(void);
     void write_pending_bytes(void);
-    uint32_t _last_write_started_us;
 };
